5c84bb9c02fe4b1022ebb63af056938ae6e89ee1
AymanAlshaikh <ayman159@live.com>
2021-03-28 14:35:37 +0300

    recipe detail working

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,10 +2,14 @@ import { Route, Switch } from "react-router";
 import Signup from "./Authentication/Signup";
 import SignIn from "./Authentication/Signin";
 import RecipeList from "./Recipe/RecipeList";
+import RecipeDetail from "./Recipe/RecipeDetail";
 
 const Routes = () => {
   return (
     <Switch>
+      <Route path="/recipes/:recipeSlug">
+        <RecipeDetail />
+      </Route>
       <Route path="/recipes">
         <RecipeList />
       </Route>
