88604e0aaa174e4b6bdbbde868ab91686645eb62
IsmailQamber <ism-you-95@hotmail.com>
2021-03-28 16:45:25 +0300

    profile view and edit done

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,12 +2,16 @@ import { Route, Switch } from "react-router";
 import Signup from "./Authentication/Signup";
 import SignIn from "./Authentication/Signin";
 import RecipeList from "./Recipe/RecipeList";
+import UserProfile from "./UserProfile";
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/recipes">
         <RecipeList />
+      </Route>
+      <Route path="/profile">
+        <UserProfile />
       </Route>
       <Route path="/signup">
         <Signup />
