394a47ed5ef43f5b5699e658fb81d842a42f6647
AymanAlshaikh <ayman159@live.com>
2021-04-04 16:59:38 +0300

    nothing important

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import SwipeableTemporaryDrawer from "./components/Drawer";
 import Routes from "./components/Routes";
 import Copyright from "./components/Authentication/Copyright";
-import AddSession from "./components/Session/AddSession";
 
 function App() {
   return (
