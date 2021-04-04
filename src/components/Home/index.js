c6b238da5a4e238c32a4a2ba8a9c99959df10c19
A-Abdeen <ahmed.yahya18@gmail.com>
2021-04-04 16:57:14 +0300

    copyright & homepage

---
+++ src/components/Home/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useStyles } from "./styles";
+
+const Home = () => {
+  const classes = useStyles();
+  const videoSource =
+    "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4";
+
+  return (
+    <div className={classes.root}>
+      <video className={classes.video} autoPlay muted loop>
+        <source src={videoSource} type="video/mp4" />
+      </video>
+      <div className={classes.content}>
+        <h3>Coming Soon</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
