e96548980dc5a7b7b222f97ebe347691c77122f3
AymanAlshaikh <ayman159@live.com>
2021-04-11 23:20:59 +0300

    chef can delete+edit from the recipe details

--- src/components/Recipe/RecipeDetail/index.js
+++ src/components/Recipe/RecipeDetail/index.js
@@ -1,18 +1,45 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect, useParams } from "react-router";
+import { Link } from "react-router-dom";
 import { useStyles } from "./styles";
-
-import { Card, CardContent, CardMedia, Typography } from "@material-ui/core/";
+import { deleteRecipe } from "../../../store/actions/recipeActions";
+import {
+  IconButton,
+  Card,
+  CardContent,
+  CardMedia,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@material-ui/core/";
+import { MoreVert } from "@material-ui/icons";
+import { useDispatch } from "react-redux";
 
 export default function RecipeDetail() {
   const classes = useStyles();
   const { recipeSlug } = useParams();
-
+  const dispatch = useDispatch();
   const recipe = useSelector((state) =>
     state.recipeReducer.recipe.find((recipe) => recipe.slug === recipeSlug)
   );
 
+  const chefs = useSelector((state) => state.chefReducer.chef);
+  const user = useSelector((state) => state.authReducer.user);
+  let chef = null;
+  if (user) {
+    chef = chefs.find((chef) => chef.userId === user.id);
+  }
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   if (!recipe) return <Redirect to="/recipes" />;
   return (
     <Card className={classes.root}>
@@ -32,6 +59,32 @@ export default function RecipeDetail() {
           Ingredients: {recipe.ingredientDescription}
         </Typography>
       </CardContent>
+      {user && user.isChef && chef.id === recipe.chefId ? (
+        <>
+          <IconButton
+            aria-controls="chef-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <MoreVert />
+          </IconButton>
+          <Menu
+            id="chef-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem component={Link} to={`/recipes/${recipeSlug}/edit`}>
+              Edit
+            </MenuItem>
+            <MenuItem onClick={() => dispatch(deleteRecipe(recipe.id, chef))}>
+              Delete
+            </MenuItem>
+          </Menu>{" "}
+        </>
+      ) : (
+        ""
+      )}
     </Card>
   );
 }
